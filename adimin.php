7e9582d416a519f1fdf70e1db92a0a5d7b0cac60
Iori <iorilima13@gmail.com>
2026-05-25 20:25:07 -0300

    aqui esta

---
+++ adimin.php
@@ -0,0 +1,19 @@
+<?php
+$titulo_da_pagina = "Admin Spotfy";
+ include 'inc-cabeca.php'; 
+ ?>
+</head>
+<body>
+     <?php include 'inc-menu.php'; ?>
+    <main class="container">
+
+        <h1 class="text-center my-5"><i class="bi bi-spotify"></i>Spotfy</h1>
+
+        <div class="row">
+            <div class="col">
+                <a href="discografia-formulario.php" class="btn btn-success">Nova Discografia</a>
+            </div>
+        </div>
+    </main>
+
+    <?php include 'inc-rodape.php'; ?>
---
+++ inc-menu.php
@@ -0,0 +1,7 @@
+ <link rel="stylesheet" href="style.css">
+<nav class="nav-topo">
+    <a href="discografia-formulario.php">Discografia</a>
+    <a href="artista-formulario.php">Artistas</a>
+    <a href="musica-formulario.php">Músicas</a>
+    <a href="gravador-formulario.php">Gravadoras</a>
+</nav>
