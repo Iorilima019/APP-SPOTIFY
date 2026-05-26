79d01bb43586089690ec51b0e99a0fa3262ac322
Iori <iorilima13@gmail.com>
2026-05-25 21:47:35 -0300

    Update inc-menu.php

--- inc-menu.php
+++ inc-menu.php
@@ -1,6 +1,6 @@
  <link rel="stylesheet" href="style.css">
 <nav class="nav-topo">
-    <a href="discografia-formulario.php">Discografia</a>
+    <a href="discografia-listagem.php">Discografia</a>
     <a href="artista-formulario.php">Artistas</a>
     <a href="musica-formulario.php">Músicas</a>
     <a href="gravador-formulario.php">Gravadoras</a>
