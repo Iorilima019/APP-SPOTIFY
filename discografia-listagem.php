c84c8dc87e166cc52fce2aa9f9cdadca75a0ebbf
Iori <iorilima13@gmail.com>
2026-05-25 21:47:39 -0300

    Create discografia-listagem.php

---
+++ discografia-listagem.php
@@ -0,0 +1,20 @@
+<?php
+$titulo_da_pagina = "Listagem de Discografias";
+ include 'inc-cabeca.php'; 
+?>
+
+<body>
+    <main class="container">
+        <?php include 'inc-menu.php'; ?>
+        <h1 class="text-center my-5"><i class="bi bi-spotify"></i>Listagem de Discografias</h1>
+        <div class="row">
+            <div class="col">
+                <a href="discografia-formulario.php" class="btn btn-success mb-3">Nova discografia</a>
+            </div>    
+        </div>
+
+        <div class="row">
+            <div class="col">
+
+    </main>
+<?php include 'inc-rodape.php'; ?>
